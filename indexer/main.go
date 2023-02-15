f5f68a4a42257ec36cd5990433d0d38ee46e2835
daoleno <daodaoonline@gmail.com>
2023-02-15 18:40:12 +0800

    Concurrent fetch logs

--- indexer/main.go
+++ indexer/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	"math/big"
+	"sync"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -19,6 +21,8 @@ type Config struct {
 	PGDSN           string `mapstructure:"pg_dsn"`
 	StartBlock      int64  `mapstructure:"start_block"`
 	Step            int64  `mapstructure:"step"`
+	StepFactor      int64  `mapstructure:"step_factor"`
+	MaxRetries      int64  `mapstructure:"max_retries"`
 }
 
 func main() {
@@ -30,7 +34,8 @@ func main() {
 
 	// fetch raw logs from ethereum
 	logsch := make(chan types.Log)
-	go fetchLogs(ethclient, db, conf.ContractAddress, conf.StartBlock, conf.Step, logsch)
+	ctx := context.Background()
+	go fetchLogs(ctx, ethclient, db, conf.ContractAddress, conf.StartBlock, conf.Step, conf.StepFactor, conf.MaxRetries, logsch)
 
 	// process logs and save to db
 	processLogs(db, logsch)
@@ -75,12 +80,20 @@ func initEthClient(rpcURL string) *ethclient.Client {
 	return ethclient
 }
 
-// fetchLogs fetch logs from ethereum and pass to logs channel
-// step is the number of blocks to fetch at a time
-func fetchLogs(ethclient *ethclient.Client, db *pgxpool.Pool, contractAddress string, startBlock, step int64, logsch chan types.Log) {
-	// fetch last block number from db, if not found, use startBlock
+// / fetchLogs fetches logs from ethereum and sends them to the logsch channel.
+// It fetches logs in batches of size `step`, starting from the `startBlock` and moving forward.
+// The `contractAddress` parameter specifies the contract address to filter logs.
+// The `maxRetries` parameter specifies the maximum number of retries if an error occurs.
+// The `concurrency` parameter specifies the maximum number of concurrent requests to make.
+func fetchLogs(ctx context.Context, ethclient *ethclient.Client, db *pgxpool.Pool, contractAddress string, startBlock, step, stepFactor, maxRetries int64, logsch chan<- types.Log) {
+	if stepFactor <= 0 {
+		log.Fatalf("stepFactor must be greater than 0")
+		return
+	}
+
+	// Fetch last block number from db, if not found, use startBlock
 	var lastBlock int64
-	err := db.QueryRow(context.Background(), `SELECT "blockNumber" FROM "LastBlock"`).Scan(&lastBlock)
+	err := db.QueryRow(ctx, `SELECT "blockNumber" FROM "LastBlock"`).Scan(&lastBlock)
 	if err != nil {
 		log.Printf("Error fetching last block number, using start block: %d", startBlock)
 	} else {
@@ -89,41 +102,69 @@ func fetchLogs(ethclient *ethclient.Client, db *pgxpool.Pool, contractAddress st
 
 	for {
 		// Fetch logs in batches of `step` blocks
-		endBlock := startBlock + step
-		logsSlice, err := ethclient.FilterLogs(
-			context.Background(),
-			ethereum.FilterQuery{
-				FromBlock: big.NewInt(startBlock),
-				ToBlock:   big.NewInt(endBlock),
-				Addresses: []common.Address{common.HexToAddress(contractAddress)},
-			})
-		if err != nil {
-			log.Fatalf("Error fetching logs from block %d to %d: %s", startBlock, endBlock, err)
+		var wg sync.WaitGroup
+		for i := int64(0); i < stepFactor; i++ {
+			wg.Add(1)
+			go func(i int64) {
+				defer wg.Done()
+				endBlock := startBlock + step*(i+1) - 1
+				if i == stepFactor-1 {
+					endBlock = startBlock + step*stepFactor - 1
+				}
+				startIndex := startBlock + step*i
+				logsSlice, err := fetchLogsInRange(ctx, ethclient, contractAddress, startIndex, endBlock, maxRetries)
+				if err != nil {
+					log.Fatalf("Error fetching logs from block %d to %d: %s", startIndex, endBlock, err)
+					return
+				}
+				log.Printf("Fetched %d logs from block %d to %d", len(logsSlice), startIndex, endBlock)
+				for _, l := range logsSlice {
+					select {
+					case logsch <- l:
+					case <-ctx.Done():
+						log.Println("Context cancelled. Exiting fetchLogs.")
+						return
+					}
+				}
+			}(i)
 		}
-
-		log.Printf("Fetched %d logs from block %d to %d", len(logsSlice), startBlock, endBlock)
-
-		// If no logs are found, move on to the next batch
-		if len(logsSlice) == 0 {
-			updateLastBlock(db, endBlock)
-			startBlock = endBlock + 1
-			continue
-		}
-
-		for _, log := range logsSlice {
-			logsch <- log
-		}
-		startBlock = endBlock + 1
+		wg.Wait()
 
 		// Upsert last block number
-		updateLastBlock(db, endBlock)
+		updateLastBlock(ctx, db, startBlock+step*stepFactor-1)
 
+		startBlock += step * stepFactor
 	}
 }
 
-func updateLastBlock(db *pgxpool.Pool, number int64) {
+// fetchLogsInRange fetches logs in the given block range and returns them in a slice.
+// The `contractAddress` parameter specifies the contract address to filter logs.
+// The `startBlock` and `endBlock` parameters specify the range of blocks to fetch logs from.
+// The `maxRetries` parameter specifies the maximum number of retries if an error occurs.
+func fetchLogsInRange(ctx context.Context, ethclient *ethclient.Client, contractAddress string, startBlock, endBlock, maxRetries int64) ([]types.Log, error) {
+	var logs []types.Log
+	var err error
+
+	for i := int64(0); i < maxRetries; i++ {
+		logs, err = ethclient.FilterLogs(ctx, ethereum.FilterQuery{
+			FromBlock: big.NewInt(startBlock),
+			ToBlock:   big.NewInt(endBlock),
+			Addresses: []common.Address{common.HexToAddress(contractAddress)},
+		})
+		if err == nil {
+			return logs, nil
+		}
+		// Log the error and wait for a while before retrying
+		log.Printf("Error fetching logs from block %d to %d: %s, retrying in 5 seconds...", startBlock, endBlock, err)
+		time.Sleep(time.Second * 5)
+	}
+
+	return nil, err
+}
+
+func updateLastBlock(ctx context.Context, db *pgxpool.Pool, number int64) {
 	// Upsert last block number
-	_, err := db.Exec(context.Background(), `INSERT INTO "LastBlock" ("id", "blockNumber") VALUES (1, $1) ON CONFLICT ("id") DO UPDATE SET "blockNumber" = $1`, number)
+	_, err := db.Exec(ctx, `INSERT INTO "LastBlock" ("id", "blockNumber") VALUES (1, $1) ON CONFLICT ("id") DO UPDATE SET "blockNumber" = $1`, number)
 	if err != nil {
 		log.Fatalf("Error upserting last block number: %s", err)
 	}
