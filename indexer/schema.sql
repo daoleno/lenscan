09a36e4d34f76740db5cb0e792ac394fbdbc8dad
daoleno <daodaoonline@gmail.com>
2023-05-01 03:02:23 +0800

    feat(schema): add get_publications_summary function

--- indexer/schema.sql
+++ indexer/schema.sql
@@ -19,3 +19,30 @@ CREATE TABLE IF NOT EXISTS "LastBlock" (
     "id" INT PRIMARY KEY,
     "blockNumber" INT
 );
+
+
+-- for lens profile
+CREATE OR REPLACE FUNCTION public.get_publications_summary(
+    profile_id bigint,
+    start_date timestamp without time zone,
+    end_date timestamp without time zone
+) RETURNS TABLE (date date, count bigint, level bigint) LANGUAGE plpgsql IMMUTABLE AS $function$
+BEGIN
+    RETURN QUERY SELECT
+        DATE_TRUNC('day', to_timestamp("timestamp"))::date AS "date",
+        COUNT(*) AS "count",
+        CASE
+            WHEN COUNT(*) > 4 THEN 4
+            ELSE COUNT(*)
+        END AS "level"
+    FROM
+        "Event"
+    WHERE
+        "type" IN ('PostCreated', 'CommentCreated', 'MirrorCreated')
+        AND "data"->>'ProfileId' = profile_id::text
+        AND to_timestamp("timestamp") >= start_date
+        AND to_timestamp("timestamp") < end_date
+    GROUP BY "date"
+    ORDER BY "date";
+END;
+$function$
