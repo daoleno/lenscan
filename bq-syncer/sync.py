d2083ff55da0a13b1fee916d815520415d02fb6b
daoleno <daodaoonline@gmail.com>
2023-11-24 16:41:52 +0800

    format code, add logs

--- bq-syncer/sync.py
+++ bq-syncer/sync.py
@@ -1,23 +1,24 @@
-import duckdb
-import schedule
 import time
-from google.cloud import bigquery
 from datetime import datetime
-from google.oauth2 import service_account
-from google.cloud import bigquery
-import polars as pl
 
+import duckdb
+import polars as pl
+import schedule
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 # Initialize BigQuery client
-service_account_path = 'service_account.json'
-credentials = service_account.Credentials.from_service_account_file(service_account_path)
+service_account_path = "service_account.json"
+credentials = service_account.Credentials.from_service_account_file(
+    service_account_path
+)
 bqclient = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-dataset_ref = bqclient.dataset('v2_polygon', project='lens-public-data')
+dataset_ref = bqclient.dataset("v2_polygon", project="lens-public-data")
 dataset = bqclient.get_dataset(dataset_ref)
 
 # DuckDB connection
-cursor = duckdb.connect(database='v2_polygon.db').cursor()
+cursor = duckdb.connect(database="v2_polygon.db").cursor()
 
 is_task_running = False
 
@@ -25,31 +26,38 @@ is_task_running = False
 def convert_schema(table_schema):
     converted_schema = []
     for field in table_schema:
-        if field.field_type == 'RECORD' and field.name == 'datastream_metadata':
+        if field.field_type == "RECORD" and field.name == "datastream_metadata":
             for subfield in field.fields:
-                if subfield.name == 'source_timestamp':
+                if subfield.name == "source_timestamp":
                     converted_schema.append(f"{subfield.name} BIGINT")
         else:
             converted_schema.append(f"{field.name} {field.field_type}")
     return converted_schema
+
 
 def perform_sync_task():
     global is_task_running
 
     # Check if task is already running
     if is_task_running:
-        print(f"[{datetime.now()}] Another sync task is already running. Skipping this cycle.")
+        print(
+            f"[{datetime.now()}] Another sync task is already running. Skipping this cycle."
+        )
         return
-    
+
     is_task_running = True
     print(f"[{datetime.now()}] Starting data sync...")
 
-    for table in bqclient.list_tables(dataset):
+    tables = list(bqclient.list_tables(dataset))  # Convert to list only once
+    total_tables = len(tables)
+    for index, table in enumerate(tables, start=1):
         table_id = table.table_id
         table_ref = dataset_ref.table(table_id)
 
         # Check if table exists in DuckDB and create if not
-        cursor.execute(f"SELECT count(*) FROM information_schema.tables WHERE table_name = '{table_id}'")
+        cursor.execute(
+            f"SELECT count(*) FROM information_schema.tables WHERE table_name = '{table_id}'"
+        )
         if cursor.fetchone()[0] == 0:
             # Fetch the table schema from BigQuery
             table_schema = bqclient.get_table(table_ref).schema
@@ -59,16 +67,20 @@ def perform_sync_task():
             cursor.execute(ddl)
 
         # Attempt to retrieve the last sync timestamp from the DuckDB table
-        last_timestamp_result = cursor.execute(f'SELECT MAX(source_timestamp) FROM {table_id}').fetchone()
-        last_timestamp = last_timestamp_result[0] if last_timestamp_result[0] is not None else 0
+        last_timestamp_result = cursor.execute(
+            f"SELECT MAX(source_timestamp) FROM {table_id}"
+        ).fetchone()
+        last_timestamp = (
+            last_timestamp_result[0] if last_timestamp_result[0] is not None else 0
+        )
 
         # Build query to fetch new or updated records from BigQuery
         # Fetch the table schema from BigQuery
         table_schema = bqclient.get_table(table_ref).schema
 
         # Generate list of fields, excluding 'datastream_metadata', but including 'datastream_metadata.source_timestamp'
-        fields = [f.name for f in table_schema if f.name != 'datastream_metadata']
-        fields.append('datastream_metadata.source_timestamp')
+        fields = [f.name for f in table_schema if f.name != "datastream_metadata"]
+        fields.append("datastream_metadata.source_timestamp")
 
         query = f"""
         SELECT {', '.join(fields)} 
@@ -81,12 +93,23 @@ def perform_sync_task():
         for page in iterator.pages:
             page_num += 1
             items = list(page)
-            df = pl.DataFrame({field.name: list(data) for field, data in zip(table_schema, zip(*items))})
+            df = pl.DataFrame(
+                {
+                    field.name: list(data)
+                    for field, data in zip(table_schema, zip(*items))
+                }
+            )
             # insert into duckdb
             if df.height > 0:  # Check if DataFrame is not empty
-                cursor.register('df', df)
-                cursor.execute(f'INSERT INTO {table_id} SELECT * FROM df')
-                print(f"[{datetime.now()}] Synced table: {table_id}, Page: {page_num}, Rows: {len(items)}")
+                cursor.register("df", df)
+                cursor.execute(f"INSERT INTO {table_id} SELECT * FROM df")
+                print(
+                    f"[{datetime.now()}] Synced table {index}/{total_tables}: {table_id}, Page: {page_num}, Rows: {len(items)}"
+                )
+            else:
+                print(
+                    f"[{datetime.now()}] No new records found for table {index}/{total_tables}: {table_id}"
+                )
 
     print(f"[{datetime.now()}] Data sync completed.")
     is_task_running = False
