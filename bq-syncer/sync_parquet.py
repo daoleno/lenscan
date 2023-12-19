5c47469e960ad0fd9ed022df8716377b1990a4dc
daoleno <daodaoonline@gmail.com>
2023-12-19 19:56:35 +0800

    feat: add concurrency support, fix parquet concatenation type casting

--- bq-syncer/sync_parquet.py
+++ bq-syncer/sync_parquet.py
@@ -3,9 +3,11 @@ import concurrent.futures
 import os
 import sys
 import time
+import traceback
 from datetime import datetime
 
 import polars as pl
+import pyarrow.parquet as pq
 import schedule
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -32,6 +34,13 @@ parser.add_argument(
     action="store_true",
     default=False,
 )
+parser.add_argument(
+    "-c",
+    "--concurrency",
+    help="Number of concurrent tasks.",
+    type=int,
+    default=1,
+)
 args = parser.parse_args()
 dataset_ref = bqclient.dataset("v2_polygon", project="lens-public-data")
 dataset = bqclient.get_dataset(dataset_ref)
@@ -41,7 +50,22 @@ output_directory = args.output
 os.makedirs(output_directory, exist_ok=True)
 
 
+def map_bq_type_to_python(bq_type):
+    type_mapping = {
+        "STRING": str,
+        "BYTES": bytes,
+        "INTEGER": int,
+        "FLOAT": float,
+        "BOOLEAN": bool,
+        "TIMESTAMP": datetime,
+    }
+    return type_mapping.get(bq_type, str)
+
+
 def sync_table(table_item, index, total_tables):
+    if table_item.table_id != "profile_metadata":
+        return
+
     last_timestamp = 0
     table_id = table_item.table_id
     table_ref = dataset_ref.table(table_id)
@@ -88,15 +112,25 @@ def sync_table(table_item, index, total_tables):
             df = pl.DataFrame({field: data for field, data in zip(fields, zip(*items))})
             if os.path.exists(parquet_file_path):
                 df_old = pl.read_parquet(parquet_file_path)
-                df = df_old.vstack(df)
+                df = df.select(
+                    [
+                        pl.col(col).cast(df_old[col].dtype)
+                        if col in df_old.columns
+                        else pl.col(col)
+                        for col in df.columns
+                    ]
+                )
+
+                df = pl.concat([df_old, df])
             df.write_parquet(parquet_file_path)
+
         print(
             f"[{datetime.now()}] Data sync of table {index}/{total_tables}: {table_id} completed."
         )
-    except Exception as table_related_error:
-        print(
-            f"An error occurred while processing table {table_id}: {table_related_error}"
-        )
+    except Exception as e:
+        # Print more verbose error information
+        print(f"An error occurred while processing table {table_id}: {e}")
+        traceback.print_exc()  # This prints the stack trace
         sys.exit(1)
 
 
@@ -116,8 +150,8 @@ def perform_sync_task():
 
     try:
         with concurrent.futures.ThreadPoolExecutor(
-            max_workers=5
-        ) as executor:  # You can adjust the max_workers to your needs
+            max_workers=args.concurrency
+        ) as executor:
             for index, table_item in enumerate(tables, start=1):
                 executor.submit(sync_table, table_item, index, total_tables)
     except Exception as e:
