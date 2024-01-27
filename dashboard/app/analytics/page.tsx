b4026d451a1fe70ab075f783be97153cd32c3050
daoleno <daodaoonline@gmail.com>
2024-01-27 11:06:25 +0800

    fix the analytics refresh issue.

--- dashboard/app/analytics/page.tsx
+++ dashboard/app/analytics/page.tsx
@@ -1,9 +1,12 @@
+import { RevalidateTime } from "@/config/ssr"
 import AllUserActivity from "@/components/charts/all-user-activity"
 import ContentLanguageDistribution from "@/components/charts/content-language-distribution"
 import DAU from "@/components/charts/dau"
 import PopularHashtags from "@/components/charts/hashtags"
 import MAU from "@/components/charts/mau"
 import PublicationTypesDistribution from "@/components/charts/publication-types-distribution"
+
+export const revalidate = RevalidateTime
 
 export default function Page() {
   return (
