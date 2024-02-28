34774dae5568398cde10a5c40b4a33314b0a2f1c
daoleno <daodaoonline@gmail.com>
2024-02-28 19:21:32 +0800

    refactor: sync to postgres instead of duckdb

--- bq-syncer/sync.py
+++ bq-syncer/sync.py
@@ -1,18 +1,35 @@
 import argparse
 import logging
-import os
-import shutil
-import time
 from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
 
-import duckdb
-import polars as pl
-import schedule
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from psycopg_pool import ConnectionPool
 
 logging.basicConfig(level=logging.INFO)
+
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Sync data from BigQuery to PostgreSQL.")
+parser.add_argument(
+    "-p", "--pg-dsn", help="PostgreSQL DSN (Data Source Name).", required=True
+)
+parser.add_argument(
+    "-c",
+    "--concurrency",
+    help="Number of concurrent tasks.",
+    type=int,
+    default=1,
+)
+parser.add_argument(
+    "-s",
+    "--sample-size",
+    help="Number of records to sample for each table (for debugging). 0 for no limit.",
+    type=int,
+    default=0,
+)
+args = parser.parse_args()
+
 
 # Initialize BigQuery client
 service_account_path = "service_account.json"
@@ -22,176 +39,115 @@ credentials = service_account.Credentials.from_service_account_file(
 bqclient = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description="Connect to DuckDB and export tables.")
-parser.add_argument(
-    "-i", "--input", help="Path to the input database file.", required=True
-)
-parser.add_argument(
-    "-o", "--output", help="Path to the directory to which exports will be saved."
-)
-parser.add_argument(
-    "-c",
-    "--concurrency",
-    help="Number of concurrent tasks.",
-    type=int,
-    default=1,
-)
-args = parser.parse_args()
-
 dataset_ref = bqclient.dataset("v2_polygon", project="lens-public-data")
 dataset = bqclient.get_dataset(dataset_ref)
-
 
 is_task_running = False
 
 
 def convert_schema(table_schema):
     converted_schema = []
+    type_mapping = {
+        "STRING": "TEXT",
+        "INTEGER": "INTEGER",
+        "FLOAT": "REAL",
+        "BOOLEAN": "BOOLEAN",
+        "TIMESTAMP": "TIMESTAMPTZ",  # Mapping TIMESTAMP to TIMESTAMPTZ
+        "NUMERIC": "NUMERIC",  # Mapping NUMERIC directly
+        "JSON": "JSONB",  # Mapping JSON to JSONB
+        # Add other known BigQuery data types as needed
+    }
+
     for field in table_schema:
         if field.field_type == "RECORD" and field.name == "datastream_metadata":
+            # Specifically handle the 'datastream_metadata' RECORD type
             for subfield in field.fields:
                 if subfield.name == "source_timestamp":
+                    # Convert 'source_timestamp' within 'datastream_metadata' to BIGINT
                     converted_schema.append(f"{subfield.name} BIGINT")
+        elif field.field_type in type_mapping:
+            # Handle other known types normally
+            converted_type = type_mapping[field.field_type]
+            converted_schema.append(f"{field.name} {converted_type}")
         else:
-            converted_schema.append(f"{field.name} {field.field_type}")
+            # Raise an error for unsupported or unknown types
+            raise ValueError(
+                f"Unsupported or unknown BigQuery data type: {field.field_type}"
+            )
+
     return converted_schema
 
 
-def create_output_directory():
-    """
-    Create a new directory with a timestamp in the specified location.
-    Return the directory name.
-    """
-    date_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_directory = os.path.join(args.output, f"v2_polygon_{date_str}")
-    os.makedirs(output_directory, exist_ok=True)
-    logging.info(f"Output directory set to: {output_directory}")
-    return output_directory
-
-
-def update_symbolic_link(output_dir):
-    """
-    Update the symbolic link to the specified target directory.
-    """
-    symlink_path = os.path.join(args.output, "v2_polygon")
-    if os.path.exists(symlink_path):
-        if os.path.islink(symlink_path):
-            os.unlink(symlink_path)
-    os.symlink(os.path.abspath(output_dir), symlink_path)
-    logging.info(f"Symbolic link updated to: {output_dir}")
-
-
-def export_tables(conn, output_dir):
-    """
-    Export all tables to Parquet files in the given directory.
-    """
+def process_table(table, pool, dataset_ref, bqclient, idx, sample_size):
     try:
-        # Retrieve a list of all tables
-        tables = conn.execute(
-            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
-        ).fetchall()
+        with pool.connection() as conn:
+            table_id = table.table_id
+            table_ref = dataset_ref.table(table_id)
+            table_schema = bqclient.get_table(table_ref).schema
+            cursor = conn.cursor()
 
-        if not tables:
-            logging.info("No tables found in the database.")
-            return
-
-        total_tables = len(tables)
-        logging.info(f"Found {total_tables} tables.")
-
-        # Export each table to a Parquet file
-        for index, table in enumerate(tables, start=1):
-            table_name = table[0]
-            parquet_file_path = os.path.join(output_dir, f"{table_name}.parquet")
-            conn.execute(
-                f"COPY {table_name} TO '{parquet_file_path}' (FORMAT 'parquet')"
-            )
-            logging.info(
-                f"Exported table {index}/{total_tables}: {table_name} to {parquet_file_path}"
-            )
-
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
-
-def delete_old_dir():
-    """
-    Delete the old directories that are not linked by the symbolic link.
-    """
-    symlink_path = os.path.join(args.output, "v2_polygon")
-    cur_link_target = os.path.realpath(symlink_path)
-    parent_dir = os.path.dirname(cur_link_target)
-    for item in os.listdir(parent_dir):
-        dir_path = os.path.join(parent_dir, item)
-        if (
-            os.path.isdir(dir_path)
-            and item.startswith("v2_polygon_")
-            and os.path.abspath(dir_path) != cur_link_target
-        ):
-            shutil.rmtree(dir_path)
-            logging.info(f"Deleted old directory: {dir_path}")
-
-
-def process_table(table, conn, dataset_ref, bqclient, idx):
-    try:
-        table_id = table.table_id
-        table_ref = dataset_ref.table(table_id)
-        table_schema = bqclient.get_table(table_ref).schema
-
-        cursor = conn.cursor()
-
-        # Check if table exists in DuckDB
-        cursor.execute(
-            f"SELECT count(*) FROM information_schema.tables WHERE table_name = '{table_id}'"
-        )
-        if cursor.fetchone()[0] == 0:
-            # Create table in DuckDB
-            logging.info(f"Creating table: {table_id}")
+            # Convert BigQuery schema to PostgreSQL schema
             converted_schema = convert_schema(table_schema)
-            ddl = f"CREATE TABLE {table_id} ({', '.join(converted_schema)})"
-            cursor.execute(ddl)
 
-        # Retrieve and sync data
-        last_timestamp_result = cursor.execute(
-            f"SELECT MAX(source_timestamp) FROM {table_id}"
-        ).fetchone()
-        last_timestamp = (
-            last_timestamp_result[0] if last_timestamp_result[0] is not None else 0
-        )
+            # Check if table exists in PostgreSQL and create it if not
+            cursor.execute(f"SELECT to_regclass('public.{table_id}');")
+            if cursor.fetchone()[0] is None:
+                logging.info(f"Creating table: {table_id}")
+                ddl = f"CREATE TABLE {table_id} ({', '.join(converted_schema)});"
+                cursor.execute(ddl)
+                conn.commit()
 
-        # Build and execute query
-        fields = [
-            f.name
-            if f.name != "datastream_metadata"
-            else "datastream_metadata.source_timestamp"
-            for f in table_schema
-        ]
-        query = f"""
-        SELECT {', '.join(fields)}
-        FROM `{table_ref}`
-        WHERE datastream_metadata.source_timestamp > {last_timestamp}
-        ORDER BY datastream_metadata.source_timestamp ASC
-        """
-        query_job = bqclient.query(query)
-        pageNum = 0
-        for page in query_job.result(page_size=10000).pages:
-            pageNum += 1
-            items = list(page)
-            if items:
-                df = pl.DataFrame(
-                    {
-                        field.name: list(data)
-                        for field, data in zip(table_schema, zip(*items))
-                    }
-                )
-                cursor.register("df", df)
-                cursor.execute(f"INSERT INTO {table_id} SELECT * FROM df")
-                logging.info(
-                    f"Synced table {idx}: {table_id} - page {pageNum} - {len(items)} rows"
-                )
-        logging.info(f"Synced table {idx}: {table_id}")
+            # Retrieve last synced timestamp
+            cursor.execute(f"SELECT MAX(source_timestamp) FROM {table_id};")
+            last_timestamp_result = cursor.fetchone()
+            last_timestamp = (
+                last_timestamp_result[0] if last_timestamp_result[0] is not None else 0
+            )
+
+            # Build and execute BigQuery query
+            fields = [
+                f.name
+                if f.name != "datastream_metadata"
+                else "datastream_metadata.source_timestamp"
+                for f in table_schema
+            ]
+
+            limit_clause = f"LIMIT {sample_size}" if sample_size > 0 else ""
+            query = f"""
+            SELECT {', '.join(fields)}
+            FROM `{table_ref}`
+            WHERE datastream_metadata.source_timestamp > {last_timestamp}
+            ORDER BY datastream_metadata.source_timestamp ASC
+            {limit_clause}
+            """
+            query_job = bqclient.query(query)
+
+            # Process and insert data into PostgreSQL
+            pageNum = 0
+            for page in query_job.result(page_size=10000).pages:
+                pageNum += 1
+                items = list(page)
+                if items:
+                    # Convert items to a format suitable for insertion into PostgreSQL
+                    # This step needs to be adapted based on your actual table schema and data types
+                    insert_values = [
+                        # Convert each item to a tuple or another format as needed for insertion
+                        tuple(item.values())
+                        for item in items
+                    ]
+                    # Generate the INSERT INTO statement with placeholders for values
+                    placeholders = ", ".join(["%s"] * len(items[0]))
+                    insert_query = f"INSERT INTO {table_id} VALUES ({placeholders});"
+                    cursor.executemany(insert_query, insert_values)
+                    conn.commit()
+                    logging.info(
+                        f"Synced table {idx}: {table_id} - page {pageNum} - {len(items)} rows"
+                    )
+
+            logging.info(f"Completed syncing table {idx}: {table_id}")
+
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        logging.error(f"An error occurred while processing table {table_id}: {e}")
     finally:
         cursor.close()
 
@@ -206,7 +162,9 @@ def perform_sync_task():
     is_task_running = True
     logging.info("Starting data sync...")
 
-    with duckdb.connect(database=args.input) as conn:
+    pg_dsn = args.pg_dsn
+    sample_size = args.sample_size
+    with ConnectionPool(conninfo=pg_dsn, min_size=1, max_size=100) as pool:
         try:
             tables = list(bqclient.list_tables(dataset))
             logging.info(f"Found {len(tables)} tables in BigQuery.")
@@ -214,14 +172,14 @@ def perform_sync_task():
             with ThreadPoolExecutor(max_workers=args.concurrency) as executor:
                 for idx, table in enumerate(tables, start=1):
                     executor.submit(
-                        process_table, table, conn, dataset_ref, bqclient, idx
+                        process_table,
+                        table,
+                        pool,  #
+                        dataset_ref,
+                        bqclient,
+                        idx,
+                        sample_size,
                     )
-
-            if args.output:
-                output_directory = create_output_directory()
-                export_tables(conn, output_directory)
-                update_symbolic_link(output_directory)
-                delete_old_dir()
 
             logging.info(f"Synced {len(tables)} tables.")
 
@@ -231,13 +189,4 @@ def perform_sync_task():
             is_task_running = False
 
 
-# Schedule the task to run every 60 minutes
-schedule.every(12).hours.do(perform_sync_task)
-
-# Run the task immediately on start
 perform_sync_task()
-
-# Run the scheduler
-while True:
-    schedule.run_pending()
-    time.sleep(1)
